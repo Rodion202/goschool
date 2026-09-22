8fc5bd6004102ff53f2b17d5a9e87cdfbb1e35fd
Rodion <rodion2009i0n@gmail.com>
2026-09-22 21:22:04 +0300

    Added chi

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import(
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func hello(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintf(w,"Nothing interesting here yet")
+	w.Write([]byte("Nothing interesting here yet but i added chi"))
 }
 
 func main(){
-	mux:=http.NewServeMux()
-	
-	mux.HandleFunc("/",hello)
+	r:=chi.NewRouter()
+
+	r.Get("/",hello)
 	
 	fmt.Println("http://localhost:8080/")
-	if err:=http.ListenAndServe(":8080",mux);err!=nil{
+	if err:=http.ListenAndServe(":8080",r);err!=nil{
 		fmt.Println("Oops:",err)
 	}
 }
